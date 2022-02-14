aa33c8ebc40e0f8c0e7a443b45d779a64118af07
Mustafa ÖZTÜRK <mustafa.ozturk@smartiks.com.tr>
2022-02-15 02:36:35 +0300

    Last edit

--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const { API_VERSION, PORT } = require("./config/env");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use("/users", require("./demo/users"));
+app.use(cors());
+
+app.use(API_VERSION + "users", require("./routes/users"));
 
 app.use(API_VERSION, require("./routes/index"));
 
